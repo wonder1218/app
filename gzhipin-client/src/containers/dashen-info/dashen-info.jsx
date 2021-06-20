14b3c94e8773f45f012487a33a00c487300347ef
wonder1218 <zhengwen_man@163.com>
2021-06-20 18:23:17 +0800

    init zp

---
+++ gzhipin-client/src/containers/dashen-info/dashen-info.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+ class DashenInfo extends Component{
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+export default connect(
+    state=>({
+
+    }),
+    {}
+)(DashenInfo)
